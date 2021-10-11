dd07d71acb81051064ae890dbe17c31ec26de611
lostgoldplayer <64813014+lostgoldplayer@users.noreply.github.com>
2021-10-11 16:52:52 -0400

    Add files via upload

--- upload.php
+++ upload.php
@@ -8,10 +8,10 @@ $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 if(isset($_POST["submit"])) {
   $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
   if($check !== false) {
-    echo "ok - " . $check["mime"] . ".";
+    echo "";
     $uploadOk = 1;
   } else {
-    echo "ok - " . $check["mime"] . ".";
+    echo "";
     $uploadOk = 1;
   }
 }
