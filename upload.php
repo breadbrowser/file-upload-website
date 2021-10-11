5d15af5bdc385338465923a4c896d7482e84ee33
lostgoldplayer <64813014+lostgoldplayer@users.noreply.github.com>
2021-10-11 16:40:50 -0400

    Add files via upload
    you need to add a folder called uploads to where ever you're putting upload.php.

---
+++ upload.php
@@ -0,0 +1,50 @@
+<?php
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+// Check if image file is a actual image or fake image
+if(isset($_POST["submit"])) {
+  $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+  if($check !== false) {
+    echo "ok - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  } else {
+    echo "ok - " . $check["mime"] . ".";
+    $uploadOk = 1;
+  }
+}
+
+// Check if file already exists
+if (file_exists($target_file)) {
+  echo "Sorry, file already exists.";
+  $uploadOk = 0;
+}
+
+// Check file size
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+  echo "Sorry, your file is too large.";
+  $uploadOk = 1;
+}
+
+// Allow certain file formats
+if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+&& $imageFileType != "gif" ) {
+  echo "";
+  $uploadOk = 1;
+}
+if ($uploadOk == 0) {
+  echo "";
+// if everything is ok, try to upload file
+} else {
+  if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    echo "The file ". htmlspecialchars( basename( $_FILES["fileToUpload"]["name"])). " has been uploaded.";
+  } else {
+    echo "Sorry, there was an error uploading your file.";
+    
+  }
+}
+?>
+
+<div id="center_button"><button onclick="location.href='http://addfiles.rf.gd/uploads/'">go to files</button></div>
